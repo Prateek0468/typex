9a75b51b1c9b7ff73effcb122ad26a1f50350d18
Prateek Bhatt <prateekbhatt.ai@gmail.com>
2026-07-15 13:35:39 +0530

    fix: enable cors frontend deploy

--- apps/server/cmd/api/main.go
+++ apps/server/cmd/api/main.go
@@ -20,8 +20,17 @@ import (
 // enable cors to talk to frontend
 func enableCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		origin := r.Header.Get("Origin")
+		allowedOrigins := map[string]bool{
+			"http://localhost:3000": true,
+			"https://typex-web-snowy.vercel.app": true,
+		}
+
+		if allowedOrigins[origin] {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
