c125f719e4ed586c83d7df57a22314190a0eb33f
Prateek Bhatt <prateekbhatt.ai@gmail.com>
2026-07-23 22:33:23 +0530

    chore: sanitize data

--- apps/server/internal/wiki/service.go
+++ apps/server/internal/wiki/service.go
@@ -2,7 +2,10 @@ package wiki
 
 import (
 	"strings"
+	"unicode"
 )
+
+const (maxTextLength = 1000)
 
 func GetRandomTypingText() (string, error) {
 	title, err := GetRandomTitle()
@@ -15,15 +18,53 @@ func GetRandomTypingText() (string, error) {
 		return "", err
 	}
 
+	text = cleanText(text)
+
 	// basic clean
 	text = strings.ReplaceAll(text, "\n", " ")
 	text = strings.Join(strings.Fields(text), " ")
 
 	// triming if the para is too long
 
-	if len(text) > 1200 {
-		text = text[:1200]
+	if len(text) > maxTextLength {
+		text = text[:maxTextLength]
 	}
 
 	return text, nil
+}
+
+
+// clean the data to only allow english, numbers etc
+func cleanText(text string) string {
+	var b strings.Builder
+
+	for _, r := range text {
+			switch {
+			case r >= 'a' && r <= 'z':
+					b.WriteRune(r)
+
+			case r >= 'A' && r <= 'Z':
+					b.WriteRune(r)
+
+			case unicode.IsDigit(r):
+					b.WriteRune(r)
+
+			case unicode.IsSpace(r):
+					b.WriteByte(' ')
+
+			case r >= 32 && r <= 126:
+					b.WriteRune(r)
+			}
+	}
+
+	text = strings.Join(strings.Fields(b.String()), " ")
+
+	if len(text) > maxTextLength {
+			text = text[:maxTextLength]
+			if idx := strings.LastIndex(text, " "); idx > 0 {
+					text = text[:idx]
+			}
+	}
+
+	return text
 }
